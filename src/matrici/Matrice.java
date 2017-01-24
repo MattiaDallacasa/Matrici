206b5685c9eeb8fde1099c794145fd981189874f
MattiaDallacasa <>
2017-01-24 20:31:46 +0100

    Matrici

---
+++ src/matrici/Matrice.java
@@ -0,0 +1,70 @@
+/*
+ * Matrice: una classe wrapper
+ * per una matrice bidimensionale di interi.
+ */
+
+package matrici;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Mattia Dallacasa
+ */
+public class Matrice {
+    
+    int[][] theMatrix;
+    
+    // costruttore uno
+    public Matrice(int x, int y) {
+        theMatrix = new int[x][y];
+    }
+    
+    // costruttore due
+    public Matrice(int[][] m) {
+        theMatrix = m;
+    }
+    
+    
+    public void fillRandom() {
+      for (int i = 0; i < theMatrix.length; i++) {
+        for (int j = 0; j < theMatrix[i].length; j++) {
+          theMatrix[i][j] = (int) (Math.random()*10);
+        }
+      }
+    }
+
+    @Override
+    public String toString() {
+        String s="";
+        for (int i = 0; i < theMatrix.length; i++) {
+            for (int j = 0; j < theMatrix[i].length; j++) { 
+                s=s+theMatrix[i][j] + "\t";
+            }
+            s += "\n";   
+        }
+        return s;
+    }
+
+    public void toFile( String filename ) {
+        try {
+            FileWriter fw = new FileWriter(filename);
+            fw.write(this.toString());
+            fw.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Matrice.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+            
+    }
+    
+
+    public void fromFile( String filename ) {
+        // da completare
+    }
+    
+}
+
---
+++ src/matrici/Matrici.java
@@ -0,0 +1,28 @@
+/*
+ * Esempi di gestione delle matrici di interi.
+ */
+
+package matrici;
+
+/**
+ *
+ * @author Mattia Dallacasa
+ */
+public class Matrici {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Matrice m = new Matrice(4, 6);
+        
+        m.fillRandom();
+        
+        System.out.println(m);
+      
+        m.toFile("data/matrice.txt");
+      
+    }
+    
+}
